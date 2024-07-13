07a1cf0633bd19d10bfbac35de872337f96cc5bd
luqmanshaban <luqmanforeal@gmail.com>
2024-07-13 22:47:59 +0300

    update

--- routes/index.js
+++ routes/index.js
@@ -6,5 +6,8 @@ const router = express.Router()
 
 router.use('/api/v1', userRouter)
 router.use('/api/v1', productRoute)
+router.get('/api/v1/hello', function async(req, res) {
+    res.status(200).json({ message: "hello, world"})
+})
 
 export default router
